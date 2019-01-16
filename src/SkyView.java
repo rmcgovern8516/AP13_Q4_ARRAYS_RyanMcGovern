1ad4834ac2f40049ba5b911afe268d5a49361387
BT_1N3_32 <rmcgovern8516@bths.edu>
2019-01-16 08:37:55 -0500

    Finished Version

--- src/SkyView.java
+++ src/SkyView.java
@@ -1,28 +1,46 @@
 public class SkyView
 {
 
+    public double[][] view;
+
     public SkyView(int numRows, int numCols, double[] scanned)
     {
-        double[][] view = new double[numRows][numCols];
-        for (int x=0; x<scanned.length; x++)
-        {
+        view = new double[numRows][numCols];
+        int x = 0;
             for (int row = 0; row<numRows; row++)
             {
                 if (row % 2 == 0)
                 {
-                    for (int col = numCols-1; col>=0; col--)
+                    for (int col = 0; col<numCols; col++)
                     {
                         view[row][col] = scanned[x];
+                        x++;
                     }
                 }
-                else if (row % 2 == 1)
+                else
                 {
                     for (int col = numCols-1; col>=0; col--)
                     {
                         view[row][col] = scanned[x];
+                        x++;
                     }
                 }
             }
+    }
+
+    public  double getAverage(int startRow, int endRow, int startCol, int endCol)
+    {
+        double output = 0;
+        int count = 0;
+        for (int row = startRow; row<=endRow; row++)
+        {
+            for (int col = startCol; col<=endCol; col++)
+            {
+                output += view[row][col];
+                count++;
+            }
         }
+        output = output/count;
+        return output;
     }
 }
--- src/SkyViewRunner.java
+++ src/SkyViewRunner.java
@@ -2,10 +2,38 @@ public class SkyViewRunner {
 
     public static void main(String[] args)
     {
+        //Create 1D and 2D Arrays
         double[] scan = {0.3,0.7,0.8,0.4,1.4,1.1,0.2,0.5,0.2,1.6,0.6,0.9};
         double[] scan2 = {0.3,0.7,0.8,0.4,1.4,1.1};
 
         SkyView sv1 = new SkyView(4,3,scan);
         SkyView sv2 = new SkyView(3,2,scan2);
+
+
+        //Print 2D Arrays
+        for(int i = 0; i<4; i++)
+        {
+            for(int j = 0; j<3; j++)
+            {
+                System.out.print(sv1.view[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for(int i = 0; i<3; i++)
+        {
+            for(int j = 0; j<2; j++)
+            {
+                System.out.print(sv2.view[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+
+        //Print Averages
+        System.out.println();
+        System.out.println("The average for SkyView1 is: "+sv1.getAverage(1,3,0,2));
+        System.out.println("The average for SkyView2 is: "+sv2.getAverage(0,1,0,1));
     }
 }
